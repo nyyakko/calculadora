25c8fb222d63ad3937d0d2c511624ec3ea745792
nyyakko <nyakonyns@gmail.com>
2023-06-25 01:44:02 -0300

    vendor locking to windows, lmao

--- calc/source/Main.cpp
+++ calc/source/Main.cpp
@@ -1,6 +1,25 @@
 #include "calc/Calc.hpp"
 
-int main()
+#if defined(_WIN32) || defined(_WIN64)
+    #define WIN32_LEAN_AND_MEAN
+    #if defined(_WIN32)
+        #define WIN32
+        #undef _AMD64_
+        #define _X86_
+    #else
+        #define WIN64
+        #define _AMD64_
+        #undef _X86_
+    #endif
+#else
+    #error This shit works only on Windows!
+#endif
+
+#include <minwindef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, PWSTR, int)
 {
     static auto constexpr FACTOR = 40;
     static auto constexpr WIDTH  = 9 * FACTOR;
